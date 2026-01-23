a28c13dfb62271bf77f8643efc8d4072f7c0f973
Stephanos Faustmann <104027187+FAUSTMANNSTEF@users.noreply.github.com>
2026-01-23 17:33:46 +0100

    Create kasa_light.py

---
+++ backend/app/devices/tp_link/kasa_light.py
@@ -0,0 +1,21 @@
+from devices.base.light import Light
+
+
+# KasaLight class wraper for tp_link SDK
+class KasaLight(Light):
+
+    def __init__(self, device):
+        self.device = device
+
+    async def turn_on(self):
+        await self.device.turn_on()
+
+    async def turn_off(self):
+        await self.device.turn_off()
+
+    async def set_brightness(self, level):
+        await self.device.set_brightness(level)
+
+    async def get_state(self):
+        state = await self.device.update()
+        return state
