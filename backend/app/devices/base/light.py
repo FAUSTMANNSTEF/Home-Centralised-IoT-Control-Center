d670c6d04cff4395bf01d99aa3a49c9faa3b3c0d
Stephanos Faustmann <104027187+FAUSTMANNSTEF@users.noreply.github.com>
2026-03-23 09:49:24 +0100

    Generic Device Implementation

--- backend/app/devices/base/light.py
+++ backend/app/devices/base/light.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
+from devices.base.device import Device
 
 
 # Abstract Light base class
-class Light(ABC):
+class Light(Device, ABC):
+
+    def __init__(self, id: str, name: str, vendor: str):
+        super().__init__(id=id, name=name, device_type="light", vendor=vendor)
 
     # Turn on
     @abstractmethod
